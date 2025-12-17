9e8f57732d98ec8e9e8f5f5ec25751e4a89e87d4
Sébastien <sebastien.schlichting@hotmail.com>
2025-12-17 16:14:04 +0100

    server: - improved the server logic
    client:
    - improved the client logic
    
    doc:
    - changed the doc to the new logic

--- client/client.py
+++ client/client.py
@@ -1,14 +1,60 @@
 import socket
+from threading import Thread
 
 HOST = '127.0.0.1'
 PORT = 8080
 
-# create a new socket object, AF_INET for IPv4 and SOCK_STREAM for TCP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
 
-# sending in bytes not strings
-sock.send(b'Hello, server!')
-print(sock.recv(1024).decode())
+class Client:
 
-sock.close()
+    def __init__(self, host, port):
+        self.sock = socket.socket()
+        self.sock.connect((host, port))
+        self.talk_to_server()
+
+        print(f"\033[34mClient:\033[0m Connected at {host}:{port}")
+
+    def talk_to_server(self):
+        Thread(target = self.receive_message).start()
+        self.send_messages()
+
+    def receive_message(self):
+        while True:
+            message = self.sock.recv(1024).decode()
+            if not message:
+                break
+            print(f"\033[33mClient:\033[0m {message}")
+
+    def send_messages(self):
+        while True:
+            client_message = input("")
+            self.sock.send(client_message.encode())
+
+Client(HOST, PORT)
+
+# # create a new socket object, AF_INET for IPv4 and SOCK_STREAM for TCP
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# sock.connect((HOST, PORT))
+#
+# # sending in bytes not strings
+# sock.send(b'Hello, server!')
+# print(sock.recv(1024).decode())
+#
+# def receive_msg():
+#     while True:
+#         el = sock.recv(1024).decode()
+#         print(el)
+#         return el
+#
+# # Creating multi thread (listening)
+# thread = threading.Thread(
+#     # What the thread will do when the thread has started
+#     target=receive_msg
+# )
+# thread.start()
+#
+# # Main Thread (talking)
+# while True:
+#     msg = input("> ")
+#     sock.send(msg)
+#     receive_msg()
--- server/server.py
+++ server/server.py
@@ -1,19 +1,58 @@
 import socket
-from server.client_handler import client_handler
+from threading import Thread
+# from server.client_handler import client_handler
 
-host = '127.0.0.1'
-port = 8080
+class Server:
 
-sock = socket.socket()
-sock.bind((host, port))
-sock.listen()
+    clients = [] # für später
 
-print(f'Server is running on {host}:{port}')
+    def __init__(self, host, port):
+        # create a new socket object, AF_INET for IPv4 and SOCK_STREAM for TCP
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((host, port))
+        self.sock.listen()
 
-# when connection: creates a new socket object (conn) and address (addr)
-conn, addr = sock.accept()
+        print(f"\033[31mServer:\033[0m startet at {host}:{port}")
 
-client_handler(conn, addr)
+        client_conn, addr = self.sock.accept()
+        self.talk_to_client(client_conn)
+
+    # method to talk to client
+    def talk_to_client(self, client_conn):
+        # args muss ein tuple sein, daemon auf true setzten damit das Programm abgewürgt werden kann
+        Thread(target = self.receive_message, args=(client_conn,), daemon=True).start()
+        self.send_message(client_conn)
+
+    # method to receive message from client
+    def receive_message(self, client_conn):
+        while True:
+            message = client_conn.recv(1024).decode()
+            print(f"\033[34mUserX:\033[0m {message}")
+            client_conn.sendall(message.encode())
+
+    # method to send message to client
+    def send_message(self, client_conn):
+        while True:
+            server_message = input("")
+            if server_message == "exit":
+                break
+            print(f"\033[31mServer:\033[0m {server_message}")
+            client_conn.send(server_message.encode())
+
+
+Server('127.0.0.1', 8080)
+
+# while True:
+#     conn, addr = sock.accept()
+#     print(f'Connected with {addr}')
+#     # handler for each client
+#     client_handler(conn, addr)
+#
+#     # start a new thread for each client
+#     thread = threading.Thread(target=client_handler, args=(conn, addr))
+#     thread.start()
+
+
 
 # while True:
 #     data = conn.recv(1024) # get data from a client in buffer size of 1024 bytes
